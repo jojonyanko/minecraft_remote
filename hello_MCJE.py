093e931e2ff9a3b52bd9c348acdd616c6a3e9637
jojonyanko <majoanma8@gmail.com>
2022-05-29 09:56:26 +0900

    main

--- hello_MCJE.py
+++ hello_MCJE.py
@@ -5,4 +5,4 @@ import param_MCJE as param
 
 mc = Minecraft.create(port=param.PORT_MC)
 mc.postToChat('Hello Minecraft Java Edition 1.16.5')
-mc.setBlocks(0, 0, 0,  -5, 127, 5,  param.GOLD_BLOCK)
+mc.setBlocks(0, 0, 0,  100, 100, 100,  param.AIR)
