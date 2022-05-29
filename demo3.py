03a291caf04fd03044259c9c7aca80e4738cda00
jojonyanko <majoanma8@gmail.com>
2022-05-29 09:56:10 +0900

    main

--- demo3.py
+++ demo3.py
@@ -1,1 +1,15 @@
 # jo
+
+from mcje.minecraft import Minecraft
+import param_MCJE as param
+
+import axis_flat
+
+mc = Minecraft.create(port=param.PORT_MC)  # MCJE:14712, MCPI:4711
+mc.postToChat("demo3")
+
+mc.postToChat("こんにちは！")
+
+axis_flat.reset_minecraft_world(mc, width=100)
+axis_flat.clear_XYZ_axis(mc, wait=0)
+axis_flat.draw_XYZ_axis(mc, wait=1)
