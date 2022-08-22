12599d1de544f10b05ae1c6ddc0ffb39ef5346b7
jojonyanko <majoanma8@gmail.com>
2022-08-22 18:33:49 +0900

    main

--- demo3.py
+++ demo3.py
@@ -19,76 +19,77 @@ mc.postToChat("こんにちは！")
 mc.setBlocks(-49,0,-49,49,200,49,param.AIR)
 
 
-mc.setBlocks(-10,1,-10,10,100,10,param.GLASS)
-mc.setBlocks(-9,1,-9,9,99,9,param.GOLD_BLOCK)
+# mc.setBlocks(-10,1,-10,10,100,10,param.GLASS)
+# mc.setBlocks(-9,1,-9,9,99,9,param.GOLD_BLOCK)
 
 
-x = 49
-z = 4
+# x = 49
+# z = 4
 
-y = 1
-for i in range(6):
+# y = 1
+# for i in range(6):
     
-    mc.setBlocks(x, y ,-z,-x,y + 7,z,param.GLASS)
-    mc.setBlocks(x, y + 1 ,-(z - 2),-x,y + 6,z - 2,param.AIR)
+#     mc.setBlocks(x, y ,-z,-x,y + 7,z,param.GLASS)
+#     mc.setBlocks(x, y + 1 ,-(z - 2),-x,y + 6,z - 2,param.AIR)
 
-    mc.setBlocks(-z, y ,x,z,y + 7,-x,param.GLASS)
-    mc.setBlocks(-(z - 2), y + 1 ,x,z - 2,y + 6,-x,param.AIR)
-    mc.setBlocks(x, y + 1 ,-(z - 2),-x,y + 6,z - 2,param.AIR)
+#     mc.setBlocks(-z, y ,x,z,y + 7,-x,param.GLASS)
+#     mc.setBlocks(-(z - 2), y + 1 ,x,z - 2,y + 6,-x,param.AIR)
+#     mc.setBlocks(x, y + 1 ,-(z - 2),-x,y + 6,z - 2,param.AIR)
 
-    y += 20
+#     y += 20
 
-y = 1
-for j in range(5):
-    x = 48
-    z = -5
-    for i in range(10):
-        mc.setBlocks(-x,y,z,-(x - 4),y + 1,z - 3,param.GLASS)
-        y += 1
-        z -= 4
+# y = 1
+# for j in range(5):
+#     x = 48
+#     z = -5
+#     for i in range(10):
+#         mc.setBlocks(-x,y,z,-(x - 4),y + 1,z - 3,param.GLASS)
+#         y += 1
+#         z -= 4
 
-    for i in range(10):
-        mc.setBlocks(-x,y,z,-(x - 4),y + 1,z - 3,param.GLASS)
-        y += 1
-        x -= 5
+#     for i in range(10):
+#         mc.setBlocks(-x,y,z,-(x - 4),y + 1,z - 3,param.GLASS)
+#         y += 1
+#         x -= 5
 
-mc.setBlocks(-3,2,-3,3,100,3,param.AIR)
-mc.setBlocks(-2,100,-2,2,101,2,param.AIR)
+# mc.setBlocks(-3,2,-3,3,100,3,param.AIR)
+# mc.setBlocks(-2,100,-2,2,101,2,param.AIR)
 
-x = -3
-y = 1
-z = -3
+# x = -3
+# y = 1
+# z = -3
 
-for j in range(4):
-    for i in range(6):
-        mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
-        x += 1
-        y += 1
+# for j in range(4):
+#     for i in range(6):
+#         mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
+#         x += 1
+#         y += 1
 
-    for i in range(6):
-        mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
-        z += 1
-        y += 1
+#     for i in range(6):
+#         mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
+#         z += 1
+#         y += 1
 
-    for i in range(6):
-        mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
-        x -= 1
-        y += 1
+#     for i in range(6):
+#         mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
+#         x -= 1
+#         y += 1
 
-    for i in range(6):
-        mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
-        z -= 1
-        y += 1
+#     for i in range(6):
+#         mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
+#         z -= 1
+#         y += 1
 
-z += 1
-x += 1
+# z += 1
+# x += 1
 
-for i in range(5):
-    mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
-    x += 1
-    y += 1
+# for i in range(5):
+#     mc.setBlock(x, y, z, param.SEA_LANTERN_BLOCK)
+#     x += 1
+#     y += 1
 
 make_kansito.make_kannsito(mc, x= -1, z= -1)
-make_kansito.make_kannsito(mc, x=1, z=1)
+make_kansito.make_kannsito(mc, x=1, z=1, y=5)
+mc.postToChat("無駄無駄無駄無駄ぁあ！！無駄なんだよジョジョォォォ！！！")
 make_kansito.make_kannsito(mc, x= -1, Z=1)
 make_kansito.make_kannsito(mc, x=1, Z= -1)
--- make_kansito.py
+++ make_kansito.py
@@ -1,20 +1,20 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
 
-def make_kannsito(mc, x=1, z=1, sityuublock=param.GLOWSTONE, kahenblock=param.GLOWSTONE, zyouhenblock=param.GLOWSTONE ):
+def make_kannsito(mc, x=1, z=1, y=76, sityuublock=param.GLOWSTONE, kahenblock=param.GLOWSTONE, zyouhenblock=param.GLOWSTONE ):
 
     blocktipe_sityu = sityuublock
     blocktipe_kahen = kahenblock
     blocktipe_zyouhen = zyouhenblock
     kudow = param.AIR
 
-    mc.setBlocks(x * 28,1, z * 28,x * 16,75,z * 16, blocktipe_sityu)
-    mc.setBlocks(x * 29,76,z * 29,x * 15,76,z * 15, blocktipe_kahen)
-    mc.setBlocks(x * 28,76,z * 28,x * 16,76,z * 16, kudow)
-    mc.setBlocks(x * 30,77,z * 30,x * 14,77,z * 14, blocktipe_kahen)
-    mc.setBlocks(x * 29,77,z * 29,x * 15,77,z * 15, kudow)
-    mc.setBlocks(x * 30,80,z * 30,x * 14,80,z * 14, blocktipe_zyouhen)
-    mc.setBlocks(x * 29,80,z * 29,x * 15,80,z * 15, kudow)
-    mc.setBlocks(x * 29,81,z * 29,x * 15,81,z * 15, blocktipe_zyouhen)
-    mc.setBlocks(x * 28,81,z * 28,x * 16,81,z * 16, kudow)
-    mc.setBlocks(x * 28,82,z * 28,x * 16,82,z * 16, blocktipe_zyouhen)
+    mc.setBlocks(x * 28,1, z * 28,x * 16,y-1,z * 16, blocktipe_sityu)
+    mc.setBlocks(x * 29,y,z * 29,x * 15,y,z * 15, blocktipe_kahen)
+    mc.setBlocks(x * 28,y,z * 28,x * 16,y,z * 16, kudow)
+    mc.setBlocks(x * 30,y+1,z * 30,x * 14,y+1,z * 14, blocktipe_kahen)
+    mc.setBlocks(x * 29,y+1,z * 29,x * 15,y+1,z * 15, kudow)
+    mc.setBlocks(x * 30,y+4,z * 30,x * 14,y+4,z * 14, blocktipe_zyouhen)
+    mc.setBlocks(x * 29,y+4,z * 29,x * 15,y+4,z * 15, kudow)
+    mc.setBlocks(x * 29,y+5,z * 29,x * 15,y+5,z * 15, blocktipe_zyouhen)
+    mc.setBlocks(x * 28,y+5,z * 28,x * 16,y+5,z * 16, kudow)
+    mc.setBlocks(x * 28,y+6,z * 28,x * 16,y+6,z * 16, blocktipe_zyouhen)
