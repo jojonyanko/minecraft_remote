543c51dc2c66fb7ac885c80e666079feb4ef8f6a
jojonyanko <majoanma8@gmail.com>
2022-09-19 20:19:44 +0900

    Mobいないverある程度完成！！

--- make_kansito.py
+++ make_kansito.py
@@ -1,23 +1,23 @@
 from mcje.minecraft import Minecraft
 import param_MCJE as param
 
-def make_kannsito(mc, x=1, z=1, y=76, sityuublock=param.GLOWSTONE, kahenblock=param.GLOWSTONE, zyouhenblock=param.GLOWSTONE ):
+def make_kannsito(mc, x=28, z=28, y=76, sityuublock=param.GLOWSTONE, kahenblock=param.GLOWSTONE, zyouhenblock=param.GLOWSTONE ):
 
     blocktipe_sityu = sityuublock
     blocktipe_kahen = kahenblock
     blocktipe_zyouhen = zyouhenblock
     kudow = param.AIR
 
-    mc.setBlocks(x * 28,1, z * 28,x * 16,y-1,z * 16, blocktipe_sityu)
-    mc.setBlocks(x * 29,y,z * 29,x * 15,y,z * 15, blocktipe_kahen)
-    mc.setBlocks(x * 28,y,z * 28,x * 16,y,z * 16, kudow)
-    mc.setBlocks(x * 30,y+1,z * 30,x * 14,y+1,z * 14, blocktipe_kahen)
-    mc.setBlocks(x * 29,y+1,z * 29,x * 15,y+1,z * 15, kudow)
-    mc.setBlocks(x * 30,y+4,z * 30,x * 14,y+4,z * 14, blocktipe_zyouhen)
-    mc.setBlocks(x * 29,y+4,z * 29,x * 15,y+4,z * 15, kudow)
-    mc.setBlocks(x * 29,y+5,z * 29,x * 15,y+5,z * 15, blocktipe_zyouhen)
-    mc.setBlocks(x * 28,y+5,z * 28,x * 16,y+5,z * 16, kudow)
-    mc.setBlocks(x * 28,y+6,z * 28,x * 16,y+6,z * 16, blocktipe_zyouhen)
+    mc.setBlocks(x,1,z,x - 12,y-1,z - 12, blocktipe_sityu)
+    mc.setBlocks(x + 1,y,z + 1,x - 13,y,z - 13, blocktipe_kahen)
+    mc.setBlocks(x,y,z,x - 12,y,z - 12, kudow)
+    mc.setBlocks(x + 2,y+1,z + 2,x - 14,y+1,z - 14, blocktipe_kahen)
+    mc.setBlocks(x + 1,y+1,z + 1,x - 13,y+1,z - 13, kudow)
+    mc.setBlocks(x + 2,y+4,z + 2,x - 14,y+4,z - 14, blocktipe_zyouhen)
+    mc.setBlocks(x + 1,y+4,z  + 1,x - 13,y+4,z - 13, kudow)
+    mc.setBlocks(x + 1,y+5,z + 1,x - 13,y+5,z - 13, blocktipe_zyouhen)
+    mc.setBlocks(x,y+5,z,x - 12,y+5,z - 12, kudow)
+    mc.setBlocks(x,y+6,z,x - 12,y+6,z - 12, blocktipe_zyouhen)
 
 def make_honto(mc,x= 10,y=100,z= 10,tyusinblock=param.GOLD_BLOCK):
     blocktipe_tyuusin = tyusinblock
