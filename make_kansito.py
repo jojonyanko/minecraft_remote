1bf8c5ad864773f37defc4f329eb2ef87ce17a84
jojonyanko <majoanma8@gmail.com>
2022-11-26 18:18:42 +0900

    main

--- make_kansito.py
+++ make_kansito.py
@@ -135,25 +135,25 @@ def make_insidekaidan_E(mc,x=3,y=1,z=-2,dansu=6,insideblock=param.SEA_LANTERN_BL
 
         make_insidekaidan_seihoukei_migi(mc,x=use_x,y=use_y,z=use_z,dansu=dansu,insideblock_seihoukei_zyouhen=blocktipe_insideblock)
 
-        # use_z += dansu
-        # use_y += dansu
+        use_z += dansu
+        use_y += dansu
 
-    #     make_insidekaidan_seihoukei_kahen(mc,x=use_x,y=use_y,z=use_z,dansu=dansu,insideblock_seihoukei_migi=blocktipe_insideblock)
+        make_insidekaidan_seihoukei_kahen(mc,x=use_x,y=use_y,z=use_z,dansu=dansu,insideblock_seihoukei_migi=blocktipe_insideblock)
 
-    #     use_x -= dansu
-    #     use_y += dansu
+        use_x -= dansu
+        use_y += dansu
 
-    #     make_insidekaidan_seihoukei_hidari(mc,x=use_x,y=use_y,z=use_z,dansu=dansu,insideblock_seihoukei_kahen=blocktipe_insideblock)
+        make_insidekaidan_seihoukei_hidari(mc,x=use_x,y=use_y,z=use_z,dansu=dansu,insideblock_seihoukei_kahen=blocktipe_insideblock)
 
-    #     use_z -= dansu
-    #     use_y += dansu
+        use_z -= dansu
+        use_y += dansu
 
-    #     make_insidekaidan_seihoukei_zyouhen(mc,x=use_x,y=use_y,z=use_z,dansu=dansu,insideblock_seihoukei_hidari=blocktipe_insideblock)
+        make_insidekaidan_seihoukei_zyouhen(mc,x=use_x,y=use_y,z=use_z,dansu=dansu,insideblock_seihoukei_hidari=blocktipe_insideblock)
 
-    #     use_x += dansu
-    #     use_y += dansu
+        use_x += dansu
+        use_y += dansu
     
-    # use_z += 1
-    # use_x += 1
+    use_z += 1
+    use_x += 1
 
-    # make_insidekaidan_seihoukei_migi(mc,x=use_x,y=use_y,z=use_z,dansu=dansu - 1,insideblock_seihoukei_zyouhen=insideblock)
+    make_insidekaidan_seihoukei_migi(mc,x=use_x,y=use_y,z=use_z,dansu=dansu - 1,insideblock_seihoukei_zyouhen=insideblock)
