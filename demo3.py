3295682bd5c5aed7d7758decef616338d86c3ff1
jojonyanko <majoanma8@gmail.com>
2022-11-26 19:24:13 +0900

    main

--- demo3.py
+++ demo3.py
@@ -76,7 +76,7 @@ mc.setBlocks(-2,100,-2,2,101,2,param.AIR)
 # #     y += 1
 
 
-make_kansito.make_insidekaidan_S(mc)
+make_kansito.make_insidekaidan_S(mc,)
 
 mc.postToChat("ハハッ")
 make_kansito.make_insidekaidan_E(mc)
